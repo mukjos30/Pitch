e9e052c3c84d2a461f9133f01edca36bb4e3feb4
mukjos30 <mukjos30@gmail.com>
2019-09-22 12:07:58 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,58 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    
+    SECRET_KEY ='josee'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://josee:Mukarange12@localhost/minutepitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+     #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://josee:Mukarange12@localhost/minutepitch'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://josee:Mukarange12@localhost/minutepitch'
+
+    DEBUG = True
+    ENV = 'development'
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY=josee
+export MAIL_USERNAME='mukjos30@gmail.com'
+export MAIL_PASSWORD='mukarange'
+
+python manage.py server
