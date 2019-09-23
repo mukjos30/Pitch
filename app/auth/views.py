0bfc5f525f6c6d680fb92ac14992126306b976a0
mukjos30 <mukjos30@gmail.com>
2019-09-23 09:35:43 +0200

    adding views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,8 +28,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        # mail_message("Welcome to One Minute Pitch","email/welcome_user",user.email,user=user)
-
+       
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
