bcdb0df49772efe3ecd2e3edf8e53acd606d2073
mukjos30 <mukjos30@gmail.com>
2019-09-27 13:19:55 +0200

    push to heroku

--- config.py
+++ config.py
@@ -6,21 +6,19 @@ class Config:
     '''
     
     
-    SECRET_KEY ='josee'
+  SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://josee:Mukarange12@localhost/minutepitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+     
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-     #  email configurations
+    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 class ProdConfig(Config):
     '''
